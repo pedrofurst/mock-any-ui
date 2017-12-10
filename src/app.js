ca78323c6392ef8818cac1870dda796b8d895cd8
Pedro Furst <pedrofurst@gmail.com>
2017-12-10 02:01:24 -0200

    feature: first version of site

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactJson from 'react-json-view'
+import 'normalize.css'
+import 'styles/index.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
+const json = {
+  "Object": {
+    "name": "pedro",
+    "age": 123
+  }
+}
+const App = () => (
+  <div className="App">
+    <div className="containeir">
+      <div className="row">
+        <div className="text-center">
+          <h1>Mock Any</h1>
+          <p>You can mock anything.</p>
+          <p>Enjoy!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default App
